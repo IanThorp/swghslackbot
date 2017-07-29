bc601bce282868545b522cea2ecbe6a93f8f1640
IanThorp <ianmthorp@gmail.com>
2017-07-28 22:30:01 -0700

    remove last route

--- hello.py
+++ hello.py
@@ -21,7 +21,7 @@ def touch_slack():
   response = requests.get(uri, params=params)
   return json.loads(response.text)['files']
 
-@app.route("/")
-if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port)
+# @app.route("/")
+# if __name__ == "__main__":
+#   port = int(os.environ.get("PORT", 5000))
+#   app.run(host='0.0.0.0', port=port)
