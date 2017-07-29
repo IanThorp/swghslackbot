1a5dbca5ba8659141e9c6656d4455a83e0b0ca4f
IanThorp <ianmthorp@gmail.com>
2017-07-28 22:27:37 -0700

    Add /slack route

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 import os
+import time
+import json
+import requests
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,6 +10,18 @@ app = Flask(__name__)
 def hello():
     return "Hello world!"
 
+@app.route("/slack")
+def touch_slack():
+  params = {
+    'token': token
+    ,'ts_to': ts_to
+    ,'count': 1000
+  }
+  uri = 'https://slack.com/api/files.list'
+  response = requests.get(uri, params=params)
+  return json.loads(response.text)['files']
+
+@app.route("/")
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
